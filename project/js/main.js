572c425b5f8aa3dfce3c17c381a62d0ec61edd13
DeniBog-script <mrwadewilson7@gmail.com>
2021-04-19 17:03:21 +0300

    new course

---
+++ project/js/main.js
@@ -0,0 +1,22 @@
+'use strict';
+const products = [
+    { id: 1, title: 'Notebook', price: 20000 },
+    { id: 2, title: 'Mouse', price: 1500 },
+    { id: 3, title: 'Keyboard', price: 5000 },
+    { id: 4, title: 'Gamepad', price: 4500 },
+    { id: 5 }
+];
+
+const renderProduct = (title = undefined, price = null) => {
+    return `<div class="product-item">
+                <h3 class="product-item__head">${title}</h3>
+                <p class="product-item__price">${price}</p>
+                <button class="by-btn">Добавить в корзину</button>
+              </div>`;
+}
+
+const renderProducts = (list) => {
+    document.querySelector('.products').innerHTML = list.map((item) => renderProduct(item.title, item.price)).join('');
+}
+
+renderProducts(products);
