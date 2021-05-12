6dd05a84020fc7f4694455967baf512d5b439dd4
DeniBog-script <mrwadewilson7@gmail.com>
2021-05-12 13:34:38 +0300

    hw7

---
+++ project_express/server/statisticRouter.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const fs = require('fs');
+const handler = require('./handler');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    fs.readFile('./server/db/statistic.json', 'utf-8', (err, data) => {
+        if (err) {
+            res.sendStatus(404, JSON.stringify({result: 0, text: err}));
+        } else {
+            res.send(data);
+        }
+    });
+});
+
+router.post('/', (req, res) => {
+    handler(req, res, 'add', './server/db/statistic.json');
+});
+// localhost:3000/api/cart/123 // req.params.id
+// localhost:3000/api/cart/?var1='sfsf'&var2='ada' // req.query
+router.put('/:id', (req, res) => {
+    handler(req, res, 'change', './server/db/statistic.json');
+});
+
+router.delete('/', (req, res) => {
+    handler(req, res, 'delete', './server/db/statistic.json');
+});
+module.exports = router;
